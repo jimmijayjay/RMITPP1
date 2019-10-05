e16dbe3e6f0a2ab4a9ddb6ca9922e9b77279821f
Yun <jaz998@users.noreply.github.com>
2019-10-05 21:46:58 +1000

    Update forgetpassword.php
    a webpage user uses to reset password, regardless whether account is already activated

--- forgetpassword.php
+++ forgetpassword.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
 
- <?php include_once('tools/head.php'); ?>
- <? include_once 'tools/forgetpassword_server.php';?>
+    <?php include_once('tools/head.php'); ?>
+    <?php include_once 'tools/forgetpassword_server.php';?>
 
   <body>
     <?php include_once('tools/nav.php'); ?>    
@@ -14,7 +14,7 @@
             <div class="probootstrap-text pt-5">
               <h1 class="probootstrap-heading text-white mb-4">Password Reset </h1>
               <div class="probootstrap-subheading mb-5">
-                <p class="h4 font-weight-normal">Enter your eamil to reset password<br><br>
+                <p class="h4 font-weight-normal">Enter your email to reset password<br><br>
                 <!--This will allow you to log into our system and see when cars are available<br><br>
                 You will need to finish your membership application and be activated before you can book --></p>
               </div>
@@ -25,10 +25,10 @@
     </section>
 
 <form action="forgetpassword.php" method="post" class="probootstrap-form mb-5">
-                
+    <?php include('tools/errors.php'); ?>                
               <div class="form-group">
                 <label for="email">Email</label>
-                <input type="email" class="form-control" id="email" name="email">
+                <input type="email" class="form-control" id="email" name="email" value="<?php echo $email; ?>">
               </div>
               <div class="form-group">
                 <input type="submit" class="btn btn-primary" id="submit" name="submit" value="Reset Password">
