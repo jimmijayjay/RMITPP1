6bdeb241c85de0acdb5524b34214da9f96b03c13
Yun <jaz998@users.noreply.github.com>
2019-10-05 21:44:00 +1000

    Create forgetpassword_server.php

---
+++ tools/forgetpassword_server.php
@@ -0,0 +1,61 @@
+<?php
+
+  include_once 'InputValidation.php';
+  include_once('db_connect.php');
+  include_once 'sendemail.php';
+  $email = "";
+  $errors = InputValidation::$errors;    
+  // Registering users
+
+  if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+      // retrieving input values from the form
+      // all emails are stored in lower case
+      // all spaces (including tabs and line ends) are removed email.
+      $email = mysqli_real_escape_string($db, preg_replace('/\s+/', '', $_POST['email']));
+      
+      //Checking whehter the email address has been registered
+      $email_check_query = "SELECT userid FROM Users WHERE email = '$email' LIMIT 1";       
+      $result = mysqli_query($db, $email_check_query);        
+      $email_exists_check = mysqli_fetch_assoc($result);     
+      if (!$email_exists_check) {
+          array_push($errors, "Email not registered");
+      }
+      
+      //If no error, sender verification link to let user reset password
+      if (count($errors) == 0) {
+            $forgetpassword_hash = md5 (rand(0,1000)); // Generate a random 32 character hash
+
+            $query = "SELECT userid FROM Users WHERE email = '$email' LIMIT 1";
+            $result = mysqli_query($db, $email_check_query); 
+            $row = mysqli_fetch_row($result);
+            $firstName = $row[0];
+            
+          
+          
+            $subject = "Car Buddy - Reset Your Password";
+            $emailMessage = '
+
+            Hi '.$firstName.',
+                
+            Your have requested resetting the password. 
+
+            Please click on the below link to reset password of your Car Buddy account:
+            http://www.carbuddy.ga/resetpassword.php?email='.$email.'&hash='.$forgetpassword_hash.'
+
+            Kind Regards,
+            Car Buddy Team
+            ';
+            sendEmail($subject, $emailMessage, $email);
+            header("Location: ./forgetpassword_success_landing.php");
+          
+      }
+  }
+
+
+
+
+
+
+
+
+?>
