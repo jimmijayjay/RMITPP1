c0cdc5ca2ff6686df4b54b6dbafe6ab5447e823e
Yun <jaz998@users.noreply.github.com>
2019-10-06 19:36:54 +1100

    Update InputValidation.php
    added password validation function - check whether two passwords input are the same and also lengh is okay.

--- tools/InputValidation.php
+++ tools/InputValidation.php
@@ -12,11 +12,22 @@ class InputValidation {
         $val = filter_var($val, FILTER_VALIDATE_EMAIL);
         if ($val === FALSE){
             return [False, "Email is not valid"];        
-        }elseif(strlen($val   ) > 100){
+        }elseif(strlen($val) > 100){
             return [False, "Email exceeds length limit"];
         }else{
             return [True, ""];
         }   
+    }
+    
+    static function password($password1, $password2){
+      if ($password1 != $password2) {
+          return [False, "The two passwords are not the same"];  
+      }elseif (strlen($password1) > 50) {
+          return [False, "Password is too long"];  
+      }
+      
+      return [True, ""];
+
     }
     
  }
