8b6e0d4539c75db1e675e90b5a905f82ea349d86
Yun <jaz998@users.noreply.github.com>
2019-10-06 19:30:15 +1100

    Update forgetpassword_success_landing.php
    wording change, and remove incldue,

--- forgetpassword_success_landing.php
+++ forgetpassword_success_landing.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-<?php include_once('tools/forgetpassword_server.php') ?>
+
 
 <?php include_once('tools/head.php'); ?>
   <body>
@@ -14,7 +14,7 @@
               <h1 class="probootstrap-heading text-white mb-4">Reset Password Email Sent</h1>
               <div class="probootstrap-subheading mb-5">
                 <p class="h4 font-weight-normal">An email will be with you shortly.<br>
-                Please click the activation link in the body to reset your password<br>
+                Please click the reset password link in the body to reset your password<br>
                 <!--This will allow you to log into our system and see when cars are available<br><br>
                 You will need to finish your membership application and be activated before you can book --></p>
               </div>
