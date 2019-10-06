52c0abcf5fd030b48c2a3f9952ab98b0fcdb9525
Yun <jaz998@users.noreply.github.com>
2019-10-06 19:29:26 +1100

    Create resetpassword_success_landing.php
    landing page and reset password is successful

---
+++ resetpassword_success_landing.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+<?php include_once('tools/head.php'); ?>
+  <body>
+    <?php include_once('tools/nav.php'); ?>    
+
+     <section class="probootstrap-cover">
+      <div class="container">
+        <div class="row probootstrap-vh-75 align-items-center text-left">
+          <div class="col-sm">
+            <div class="probootstrap-text pt-5">
+              <h1 class="probootstrap-heading text-white mb-4">Password Reset Successfully</h1>
+              <div class="probootstrap-subheading mb-5">
+                <p class="h4 font-weight-normal">Your password has been resent.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+  
+
+
+    <?php include_once('tools/footer.php'); ?>  
+
+
+    <script src="js/jquery-3.2.1.slim.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/main.js"></script>
+    
+  </body>
+</html>
