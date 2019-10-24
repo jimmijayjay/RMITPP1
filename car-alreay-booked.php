e5ec62ec8a41dcf51fb3a410f094c30897d5b66f
Yun <jaz998@users.noreply.github.com>
2019-10-24 17:59:37 +1100

    Create car-alreay-booked.php
    if on booking2.php, the validation shows car already booked for period it will be redirected to this page.

---
+++ car-alreay-booked.php
@@ -0,0 +1,128 @@
+<?php include_once('includes/header.php'); ?>
+
+     <section class="probootstrap-cover">
+      <div class="container">
+        <div class="row probootstrap-vh-75 align-items-center text-left">
+          <div class="col-sm">
+            <div class="probootstrap-text pt-5">
+              <h1 class="probootstrap-heading text-white mb-4">Car Booking Error</h1>
+              <div class="probootstrap-subheading mb-5">
+                <p class="h4 font-weight-normal">This car has just been booked by another user. Please <a href="booking.php">book another time</a>.</u></p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="probootstrap-section">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-7 pr-md-5 pr-0">
+            <h2 class="mb-5">Our Mission</h2>
+            <p>Car Buddy is a web-based program that allows users to hire vehicles of their choice which are freely parked around the city.</p>
+
+            <p>Our aim is to provide users with travel around the city with the use of bookable vehicles which can then be used by customers around the city.</p>
+
+            <p>Once customer have booked a vehicle, they will have the ability to use that vehicle within the booked period and then return the vehicle to selected Car Buddy locations.</p>
+
+            <p></p>
+
+            <p></p>
+          </div>
+          <div class="col-md-5">
+            <h2 class="mb-5">Why Us</h2>
+
+            <div class="media mb-5">
+              <div class="probootstrap-icon"><span class="icon-fingerprint display-4"></span></div>
+              <div class="media-body">
+                <h5 class="mt-0">Family Cars</h5>
+                <p>Check out our range of family cars, including a range of 7 seat SUV's and People Movers </p>
+              </div>
+            </div>
+
+            <div class="media mb-4">
+              <div class="probootstrap-icon"><span class="icon-users display-4"></span></div>
+              <div class="media-body">
+                <h5 class="mt-0">Utility Vehicles</h5>
+                <p>Need to move a load, or maybe get something that can tow with 4x4. We have a range of single and Double cab options</p>
+              </div>
+            </div>
+
+            <div class="media mb-5">
+              <div class="probootstrap-icon"><span class="icon-chat display-4"></span></div>
+              <div class="media-body">
+                <h5 class="mt-0">New Releases Every Week</h5>
+                <p>From vans to 2 door compacts, we're constantly growing our fleet</p>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="probootstrap-section probootstrap-section-feature pb-0 probootstrap-overflow-hidden">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6 pl-md-5 pb-5 order-md-2 order-1">
+            <h2 class="mb-4 display-5 probootstrap-heading">Phone Friendly</h2>
+            <div class="probootstrap-item mb-4">
+              <h3>Progressive Web App </h3>
+              <p>Our website is built as a PWA. This allows you to use our site like an app, but constantly have the latest version and data without an update required </p>
+            </div>
+            <div class="probootstrap-item mb-4">
+              <h3>Keep Updated</h3>
+              <p>Later versions of the site will include reminder notifications </p>
+            </div>
+            <div class="probootstrap-item mb-4">
+              <h3>New Releases Every Week</h3>
+              <p>Check out our ever growing fleet of cars</p>
+            </div>
+          </div>
+
+          <div class="col-md-6 pr-md-5 order-md-1 order-2">
+            <div class="probootstrap-device">
+              <img src="images/phone_3.png" alt="Free Bootstrap 4 Template by uicookies.com" class="img-fluid mb-md-0 mb-5">
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </section>
+
+    <section class="probootstrap-section">
+      <div class="container">
+        <div class="row">
+          <div class="col-md">
+            <div class="media">
+              <div class="probootstrap-icon"><span class="icon-fingerprint display-4"></span></div>
+              <div class="media-body">
+                <h5 class="mt-0">Secure</h5>
+                <p>Multi Layered Password Encryption</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md">
+            <div class="media">
+              <div class="probootstrap-icon"><span class="icon-users display-4"></span></div>
+              <div class="media-body">
+                <h5 class="mt-0">24 Hour Support</h5>
+                <p>Phone our helpdesk if you have any issues</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md">
+            <div class="media">
+              <div class="probootstrap-icon"><span class="icon-chat display-4"></span></div>
+              <div class="media-body">
+                <h5 class="mt-0">Print your Invoices</h5>
+                <p>Print or email your invoices</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+<?php include_once('includes/footer.php'); ?>
