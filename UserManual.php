2263f4a56b6f617cd9bf3176c7ef7315949f39ee
s3609382 <s3609382@student.rmit.edu.au>
2019-11-03 17:10:13 +1100

    User Manual
    Creation of the Car Buddy User Manual

---
+++ UserManual.php
@@ -0,0 +1,182 @@
+<?php include_once('includes/header.php'); ?>
+
+<section class="probootstrap-cover">
+<div class="container">
+<div class="row probootstrap-vh-75 align-items-center text-left">
+<div class="col-sm">
+<div class="probootstrap-text pt-5">
+<h1 class="probootstrap-heading text-white mb-4">Car User Manual</h1>
+<div class="probootstrap-subheading mb-5">
+<p class="h4 font-weight-normal">Learn how to use Car Buddy below</u></p>
+</div>
+</div>
+</div>
+</div>
+</div>
+</section>
+
+<section class="probootstrap-section">
+<div class="container">
+<div class="row">
+<div class="col-md-7 pr-md-5 pr-0">
+<h2 class="mb-5">User Manual</h2>
+<h3>Sign Up / Register</h3>
+<p>Step 1: Open www.carbuddy.ga in my web browser</p>
+<img src=images/UserManualImages/WebsiteLink.png><br>
+<img src=images/UserManualImages/HomePage.png width="600">
+<p>Step 2: Select the ‘Sign Up’ to open the Car Buddy Registration Page</p>
+<img src=images/UserManualImages/SignUpNav.png height="60"><br>
+<img src=images/UserManualImages/SignUpPage.png width="600"><br>
+<img src=images/UserManualImages/SignUpInput.png width="600">
+<p>Step 3: Input the following account details to register a Car Buddy account:</p>
+<p> - First Name</p>
+<p> - Last Name</p>
+<p> - Email Address</p>
+<p> - Password</p>
+<p> - Password Confirmation</p>
+<p>Once inputted, select the ‘Register’ button to create your account.</p>
+<img src=images/UserManualImages/SignUpInputtedText.png width="600"><br>
+<p>Step 4: Once the ‘Register’ button has been clicked, Car Buddy will proceed to send a confirmation email to the inputted email address where the user will need to verify the email address to complete the registration.</p>
+<img src=images/UserManualImages/SignUpVerification.png width="600"><br>
+<h3>Forgot Password</h3>
+
+<p>Step 1: Open www.carbuddy.ga in my web browser</p>
+<img src=images/UserManualImages/WebsiteLink.png>
+<p>Step 2: Select the ‘Log In’ to open the Car Buddy Login Page</p>
+<img src=images/UserManualImages/LogInNav.png><br>
+<img src=images/UserManualImages/HomePage.png width="600"><br>
+<img src=images/UserManualImages/LogInInput.png width="600"><br>
+<p>Step 3: Select the ‘Forget Password’ button</p>
+<img src=images/UserManualImages/ForgotPassword.png width="600"><br>
+<p>Step 4: The system will bring you to the Password Reset page. The user will need to input the email address link to their Car Buddy account into the ‘Email’ text box.</p>
+<img src=images/UserManualImages/PasswordResetPage.png width="800"><br>
+<p>Step 5: Once the user has inputted their email related to their account, the user will need to select the ‘Reset Password’ button</p>
+<img src=images/UserManualImages/PasswordResetPageText.png width="800"><br>
+
+
+<h3>Logging In</h3>
+<p>Open www.carbuddy.ga in my web browser</p>
+<img src=images/UserManualImages/WebsiteLink.png height="60"><br>
+<img src=images/UserManualImages/HomePage.png width="600"><br>
+<p>Step 2: Select the ‘Log In’ to open the Car Buddy Login Page</p>
+<img src=images/UserManualImages/LogInNav.png height="60"><br>
+<img src=images/UserManualImages/LogInPage.png width="600"><br>
+<img src=images/UserManualImages/LogInInput.png width="600"><br>
+<p>Step 3: Input your Login Credentials</p>
+<img src=images/UserManualImages/LogInInputtedText.png width="600"><br>
+<p>Step 4: Select the ‘Login’ button when login credentials are filled in. After the login has been pressed, the system will sign the user in if the credentials are correct.</p>
+<img src=images/UserManualImages/LogInInputtedTextButton.png width="600"><br>
+
+
+<h3>Accessing the Account Page</h3>
+<p>Open www.carbuddy.ga in my web browser</p>
+<img src=images/UserManualImages/WebsiteLink.png><br>
+<img src=images/UserManualImages/HomePage.png width="600"><br>
+<p>Step 2: Select the ‘Log In’ to open the Car Buddy Login Page</p>
+<img src=images/UserManualImages/LogInNav.png><br>
+<img src=images/UserManualImages/LogInPage.png width="600"><br>
+<img src=images/UserManualImages/LogInInput.png width="600"><br>
+<p>Step 3: Input your Login Credentials</p>
+<img src=images/UserManualImages/LogInInputtedText.png width="600"><br>
+<p>Step 4: Select the ‘Login’ button when login credentials are filled in. After the login has been pressed, the system will sign the user in if the credentials are correct.</p>
+<img src=images/UserManualImages/LogInInputtedTextButton.png width="600"><br>
+<p>Step 5: Select the ‘Account’ button to open the account page</p>
+<img src=images/UserManualImages/AccountNav.png height="60"><br>
+<img src=images/UserManualImages/AccountPage.png width="600"><br>
+<img src=images/UserManualImages/AccountPageInput.png width="600"><br>
+
+
+<h3>Change Account Password</h3>
+<p>Open www.carbuddy.ga in my web browser</p>
+<img src=images/UserManualImages/WebsiteLink.png><br>
+<img src=images/UserManualImages/HomePage.png width="600"><br>
+<p>Step 2: Select the ‘Log In’ to open the Car Buddy Login Page</p>
+<img src=images/UserManualImages/LogInNav.png><br>
+<img src=images/UserManualImages/LogInPage.png width="600"><br>
+<img src=images/UserManualImages/LogInInput.png width="600"><br>
+<p>Step 3: Input your Login Credentials</p>
+<img src=images/UserManualImages/LogInInputtedText.png width="600"><br>
+<p>Step 4: Select the ‘Login’ button when login credentials are filled in. After the login has been pressed, the system will sign the user in if the credentials are correct.</p>
+<img src=images/UserManualImages/LogInInputtedTextButton.png width="600"><br>
+<p>Step 5: Select the ‘Update Details’ button to open the account page</p>
+<img src=images/UserManualImages/AccountNav.png><br>
+<img src=images/UserManualImages/AccountPage.png width="600"><br>
+<img src=images/UserManualImages/AccountPageInput.png width="600"><br>
+<p>Step 6: The user will need to input their new password into the ‘Password’ and ‘Confirm Password’ then select the ‘Update Details’ button to update my password. If both passwords are correct the system will change their password to the inputted password.</p>
+<img src=images/UserManualImages/ChangePassword.png width="600"><br>
+
+
+<h3>Create a Booking</h3>
+<p>Open www.carbuddy.ga in my web browser</p>
+<img src=images/UserManualImages/WebsiteLink.png><br>
+<img src=images/UserManualImages/HomePage.png width="600"><br>
+<p>Step 2: Select the ‘Log In’ to open the Car Buddy Login Page</p>
+<img src=images/UserManualImages/LogInNav.png><br>
+<img src=images/UserManualImages/LogInPage.png width="600"><br>
+<img src=images/UserManualImages/LogInInput.png width="600"><br>
+<p>Step 3: Input your Login Credentials</p>
+<img src=images/UserManualImages/LogInInputtedText.png width="600"><br>
+<p>Step 4: Select the ‘Login’ button when login credentials are filled in. After the login has been pressed, the system will sign the user in if the credentials are correct.</p>
+<img src=images/UserManualImages/LogInInputtedTextButton.png width="600"><br>
+<p>Step 5: Select the ‘Make a Booking’ link to open the booking page</p>
+<img src=images/UserManualImages/BookingNav.png><br>
+<img src=images/UserManualImages/BookingPage.png width="600"><br>
+<img src=images/UserManualImages/BookingMapPage.png width="600"><br>
+<p>Step 6: In order to create a booking in Car Buddy’s system, you will need to select your location where you would like to pick your Car Buddy vehicle. This is done by selecting one of the pins located on Car Buddy’s booking map to select which car you would like to book. As seen in the reference images below:</p>
+<img src=images/UserManualImages/BookingMapPopUpPage.png width="600"><br>
+<img src=images/UserManualImages/BookingMapPopUpClosePage.png>
+<p>Step 7: Once a location has been selected, you will need to proceed to click the ‘Book’ button within the pop-up window. Once the book button has been selected, the system will present you with the next booking screen where users will be able to choose the time and date for their Car Buddy booking. As seen in the reference image below:</p>
+<img src=images/UserManualImages/BookingTimesPage.png>
+<p>Step 8:  From the screen shown above users will be able to select the time and date for their Car Buddy booking, as seen below:</p>
+<img src=images/UserManualImages/BookingTimesPickedPage.png>
+<p>Step 9: Once the user has finished selecting the time and date for the booking, they may proceed to selecting the ‘Submit’ button to conform their booking. Once the submit button has been selected, the system will present the user with the checkout page. As seen below:</p>
+<img src=images/UserManualImages/BillingDetails1.png width="600"><br>
+<img src=images/UserManualImages/BillingDetails2.png width="600"><br>
+<img src=images/UserManualImages/BillingDetails3.png width="600"><br>
+<p>Step 10: The checkout page allows the user to input their billing details in future bookings, the checkout page also prompts the user to input their credit card details which will be used as payment. Once all the details have been filled out, the user will need to press the ‘Continue to checkout’ button to finalise the booking.</p>
+
+
+<h3>Access your Booking History</h3>
+<p>Open www.carbuddy.ga in my web browser</p>
+<img src=images/UserManualImages/WebsiteLink.png><br>
+<img src=images/UserManualImages/HomePage.png width="600"><br>
+<p>Step 2: Select the ‘Log In’ to open the Car Buddy Login Page</p>
+<img src=images/UserManualImages/LogInNav.png><br>
+<img src=images/UserManualImages/LogInPage.png width="600" width="600"><br>
+<img src=images/UserManualImages/LogInInput.png width="600" width="600"><br>
+<p>Step 3: Input your Login Credentials</p>
+<img src=images/UserManualImages/LogInInputtedText.png width="600"><br>
+<p>Step 4: Select the ‘Login’ button when login credentials are filled in. After the login has been pressed, the system will sign the user in if the credentials are correct.</p>
+<img src=images/UserManualImages/LogInInputtedTextButton.png width="600"><br>
+<p>Step 5: Select the ‘Account’ button to open the account page</p>
+<img src=images/UserManualImages/AccountNav.png><br>
+<img src=images/UserManualImages/AccountPage.png width="600"><br>
+<img src=images/UserManualImages/AccountPageInput.png width="600"><br>
+<p>Step 6: Select the ‘Booking History’ link to access your Car Buddy Booking History. </p>
+<img src=images/UserManualImages/BookingHistoryLink.png width="600"><br>
+<p>Step 7: The system will present me with your Car Buddy Booking History as seen below:  </p>
+<img src=images/UserManualImages/BookingHistoryDisplay.png width="600"><br>
+<p>  </p>
+
+<h3>Return your Booked Car</h3>
+<p>  </p>
+<p>  </p>
+<p>  </p>
+<p>  </p>
+<p>  </p>
+<p>  </p>
+<p>  </p>
+<p>  </p>
+<p>  </p>
+<p>  </p>
+<p>  </p>
+<p>  </p>
+<p>  </p>
+<p>  </p>
+<p>  </p>
+<p>  </p>
+</div>
+
+</section>
+
+<?php include_once('includes/footer.php'); ?>
